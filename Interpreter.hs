b914263daf74807333717a71d700522ecfd680fc
Arash Rouhani <miffoljud@gmail.com>
2011-03-04 16:37:56 +0100

    Attempt using whnf now instead

--- Interpreter.hs
+++ Interpreter.hs
@@ -28,11 +28,11 @@ import Env
 --   The IO contains no side effects, it's safe to use unsafePerformIO on it.
 interpret :: Program -> IO (Either ErrorMessage Integer)
 interpret (Prog defs) = case mainExp of
-                          Just exp -> return $ fixIt $ runMonad env (calcExp exp)
+                          Just exp -> return $ fixIt $ runMonad env (whnf exp)
                           Nothing  -> return $ Left "no main is defined"
   where env     = defsToEnvironment defs
         mainExp = envLookup (Ident "main") env
-        fixIt   = fmap (\(VInt i) -> i)
+        fixIt   = fmap (\(EInteger i) -> i)
 
 ----------------------------- Value -------------------------------  
 
@@ -40,40 +40,35 @@ data Value = VLambda Ident Exp
            | VInt Integer
 
 liftIntOp :: (Integer -> Integer -> Integer) -> 
-             (MyMonad Value -> MyMonad Value -> MyMonad Value)
+             (MyMonad Exp -> MyMonad Exp -> MyMonad Exp)
 liftIntOp op mv1 mv2 = do 
-  (VInt i1) <- mv1   
-  (VInt i2) <- mv2   
-  return $ VInt $ i1 `op` i2
+  (EInteger i1) <- mv1   
+  (EInteger i2) <- mv2   
+  return $ EInteger $ i1 `op` i2
 
 ----------------------------- Substituting -------------------------------  
 
 
 ----------------------------- Interpreting -------------------------------  
 
-calcExp :: Exp -> MyMonad Value
-calcExp e = case e of
-  ELambda id exp        -> return (VLambda id exp)
-  EApply eFubn eArg     -> do
-    VLambda id eBody <- calcExp eFun
-    local (addBinding id eArg) (calcExp eBody)    
+whnf :: Exp -> MyMonad Exp
+whnf e = case e of
+  ELambda id exp        -> return $ ELambda id exp
+  EApply eFun eArg      -> do
+    ELambda id eBody <- whnf eFun
+    local (addBinding id eArg) (whnf eBody)    
   EIfElse eCond e1 e2   -> do
-    VInt b <- calcExp eCond
-    calcExp (if b /= 0 then e1 else e2)    
-  EPlus e1 e2           -> liftIntOp (+) (calcExp e1) (calcExp e2)
-  EMinus e1 e2          -> liftIntOp (-) (calcExp e1) (calcExp e2)
-  ELessThan e1 e2       -> fail "< not defined"
-  EInteger n            -> return $ VInt n
+    EInteger b <- whnf eCond
+    whnf (if b /= 0 then e1 else e2)    
+  EPlus e1 e2           -> liftIntOp (+) (whnf e1) (whnf e2)
+  EMinus e1 e2          -> liftIntOp (-) (whnf e1) (whnf e2)
+  ELessThan e1 e2       -> liftIntOp (intLessThan) (whnf e1) (whnf e2)
+  EInteger n            -> return $ EInteger n
   EIdent id             -> do
     mExp <- asks $ envLookup id
     case mExp of
-      Just exp -> calcExp exp
+      Just exp -> whnf exp
       Nothing  -> fail "variable was unbound when looking up"
 
-
-{-
-whnf :: Exp -> MyMonad Exp
-whnf (EApply (ELambda id eBody) eArg) = e
-whnf e = e
--}
+  where intLessThan i1 i2 = toInteger . fromEnum $ i1 < i2
 
